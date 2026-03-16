3defcbb6db2dd1b9722567af56d39fd95c302a30
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-03-16 20:15:47 -0300

    feat: create logout route

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,64 +1,71 @@
 import os
 from datetime import datetime, timedelta, timezone
+
 import jwt
-
-from flask import Blueprint, jsonify, request
 from dotenv import load_dotenv
-
-load_dotenv()
+from flask import Blueprint, jsonify, request
 
 from services.user_service import UsuarioService
 
-usuario_bp = Blueprint("usuario_bp", __name__)
+load_dotenv()
+
+
+usuario_bp = Blueprint('usuario_bp', __name__)
 
 
 # Rota de teste para verificar a conexão com o banco de dados
-@usuario_bp.route("/")
+@usuario_bp.route('/')
 def index():
     total = UsuarioService.contar_usuarios()
-    return f"Conectado ao banco! Total de usuários: {total}"
+    return f'Conectado ao banco! Total de usuários: {total}'
 
 
 # Rota para criação de novo usuário
-@usuario_bp.route("/register", methods=["POST"])
+@usuario_bp.route('/register', methods=['POST'])
 def register():
     dados = request.json
     usuario = UsuarioService.criar_usuario(
-        dados["nome"], dados["email"], dados["senha"]
+        dados['nome'], dados['email'], dados['senha']
     )
-    return jsonify({"message": "Usuário criado!", "id": usuario.id}), 201
+    return jsonify({'message': 'Usuário criado!', 'id': usuario.id}), 201
 
 
 # Rota para autenticação de usuário
-@usuario_bp.route("/login", methods=["POST"])
+@usuario_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
 
-    if not data or not data.get("email") or not data.get("senha"):
-        return jsonify({"error": "Email e senha são obriatórios"}), 400
+    if not data or not data.get('email') or not data.get('senha'):
+        return jsonify({'error': 'Email e senha são obriatórios'}), 400
 
-    usuario = UsuarioService.autenticar_usuario(data["email"], data["senha"])
+    usuario = UsuarioService.autenticar_usuario(data['email'], data['senha'])
 
     if usuario:
         payload = {
-            "user_id": str(usuario.id),
-            "exp": datetime.now(timezone.utc) + timedelta(hours=24)
+            'user_id': str(usuario.id),
+            'exp': datetime.now(timezone.utc) + timedelta(hours=24),
         }
 
-        token = jwt.encode(payload, os.getenv("JWT_KEY"), algorithm="HS256")
+        token = jwt.encode(payload, os.getenv('JWT_KEY'), algorithm='HS256')
 
         return (
             jsonify(
                 {
-                    "Message": "Login bem-sucedido",
-                    "token": token,
-                    "User": {
-                        "nome": usuario.nome,
-                        "email": usuario.email,
+                    'Message': 'Login bem-sucedido',
+                    'token': token,
+                    'User': {
+                        'nome': usuario.nome,
+                        'email': usuario.email,
                     },
                 }
             ),
             200,
         )
 
-    return jsonify({"error": "Credenciais inválidas"}), 401
+    return jsonify({'error': 'Credenciais inválidas'}), 401
+
+
+# Rota de logout
+@usuario_bp.route('/logout', methods=['POST'])
+def logout():
+    return jsonify({'message': 'Logout bem-sucedido!'}), 200
