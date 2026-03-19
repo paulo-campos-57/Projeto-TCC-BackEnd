cad96657bd0a32a93fe1c83fda85b4ef0e557c23
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-03-19 19:06:38 -0300

    feat: create route to begin game

--- src/routes/bairro_routes.py
+++ src/routes/bairro_routes.py
@@ -5,6 +5,7 @@ from services.bairro_service import BairroService
 bairro_bp = Blueprint('bairro_bp', __name__)
 
 
+# Rota para encontrar bairro por ID
 @bairro_bp.route('/<int:id_bairro>', methods=['GET'])
 def get_bairro(id_bairro):
     bairro_obj = BairroService.gerar_configuracao_bairro(id_bairro)
@@ -15,6 +16,7 @@ def get_bairro(id_bairro):
     return jsonify(bairro_obj.to_dict()), 200
 
 
+# Rota para iniciar sessão (antes do jogo)
 @bairro_bp.route('/iniciar_sessao', methods=['POST'])
 def iniciar_sessao():
     data = request.get_json()
@@ -36,6 +38,36 @@ def iniciar_sessao():
     )
 
 
+# Rota para inicar jogo
+@bairro_bp.route('/iniciar_jogo', methods=['POST'])
+def iniciar_jogo():
+    data = request.get_json()
+    bairro_id = data.get('bairroId')
+    tempo = data.get('tempoDeJogo')
+
+    if not bairro_id or not tempo:
+        return jsonify({'error': 'Dados incompletos para iniciar o jogo'}), 400
+
+    bairro = BairroService.buscar_por_id(bairro_id)
+    if not bairro:
+        return jsonify({'error': 'Bairro inválido'}), 404
+
+    return (
+        jsonify(
+            {
+                'redirect_url': '/TelaDeJogoCadastro',
+                'game_config': {
+                    'bairro': bairro.to_dict(),
+                    'tempoDeJogo': tempo,
+                    'inicio_timestamp': '2026-03-19T18:00:00',
+                },
+            }
+        ),
+        200,
+    )
+
+
+# Rota para listar todos os bairros
 @bairro_bp.route('/lista', methods=['GET'])
 def listar_bairros():
     bairros = BairroService.listar_todos()
